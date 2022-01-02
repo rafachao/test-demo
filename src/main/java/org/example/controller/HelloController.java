4bdc0ec6395d1f078927617f27b1d5cdc41dcf5a
chao windows server one <windowsServerOne@chao.com>
2022-01-02 09:05:28 +0800

    testing分支的修改

--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello!hello!";
+        return "hello!hello!666";
     }
 }
